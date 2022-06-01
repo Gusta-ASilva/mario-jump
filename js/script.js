a227edb7bc4b5f6155f6bccc82124788cc7ef9aa
Gusta-ASilva <gustavob.d.d@gmail.com>
2022-06-01 19:25:37 -0300

    configurando teclas para pular

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
 const jump =() => {
+    var tecla = window.event.keyCode;
+
+    if(tecla == 32 || tecla == 38){
     mario.classList.add('jump');
 
     setTimeout(() =>{
     
     mario.classList.remove('jump');   
 
-    },500)
+    },500) }
 }
 
 const loop = setInterval(() =>{
@@ -19,9 +22,18 @@ const loop = setInterval(() =>{
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 ){
         pipe.style.animation = 'none'
         pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = 'image/game-over.png';
+        mario.style.width = "80px";
+        mario.style.marginLeft = '38px'
+
+        clearInterval(loop);
     }
 
 },10)
 
 document.addEventListener('keydown', jump); 
-document.addEventListener('click', jump);
+document.addEventListener('touchmove', jump);
